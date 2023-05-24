a3222f1bb4fbf1cd2eafe2d2888222ba691d3540
bhaigi <31609621+bhaigi@users.noreply.github.com>
2023-05-24 13:47:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
 <body>
 
 <h2>Redirect to a Webpage</h2>
-<p>The replace() method replaces the current document with a new one:</p>
-
-<button onclick="myFunction()">Replace document</button>
-
-<script>
-function myFunction() {
-  location.replace("https://www.w3schools.com/?n=<?php echo $nav; ?>")
-}
-</script>
 
 </body>
 </html> 
